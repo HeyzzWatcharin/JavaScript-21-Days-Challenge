6cef1cc06f9193b5e4161c625db667004062c06e
Heyzzwatcharin <watcharinrachadet123@gmail.com>
2020-04-17 15:30:49 +0700

    Finish  02

--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,30 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const second = 1000;
+  const min = second * 60;
+  const hour = min * 60;
+  const day = hour *24 ;
 
+  function setelment(id,text){
+    const element = document.getElementById(id);
+    element.innerText = text;  
+  }
+
+  function countDown(){
+    const now = new Date().getTime(); //ดึงเวลาปัจจุบันออกมา
+    const newYear = new Date('December 31, 2020 00:00:00').getTime();
+    const unixTimeLeft = newYear - now;
+    setelment('days', Math.floor(unixTimeLeft / day));
+    setelment('hours', Math.floor(unixTimeLeft % day / hour));
+    setelment('minutes', Math.floor(unixTimeLeft % hour / min));
+    setelment('seconds', Math.floor(unixTimeLeft % min / second));
+  }
+
+  function run(){
+    countDown();
+    setInterval(countDown, second); //run in second time
+  }
+
+  run();
 })();
+
+
