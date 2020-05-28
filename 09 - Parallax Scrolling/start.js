f72c2e655659806015358032dc8b1dfa65b17a7c
Heyzzwatcharin <watcharinrachadet123@gmail.com>
2020-05-28 08:00:18 +0700

    complete 09

--- 09 - Parallax Scrolling/start.js
+++ 09 - Parallax Scrolling/start.js
@@ -1,4 +1,16 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function onscroll(){
+    const moonElem = document.querySelector('.moon');
+    const wishElem = document.querySelector('.wish'); //add class wish
 
+    moonElem.style.transform =  `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`; //Set style
+    wishElem.style.transform =  `translateY(${window.scrollY * -1.2}%)`; //Set style
+  }
+
+  function run(){
+    document.addEventListener('scroll',onscroll);
+  }
+
+  run();
 })();
