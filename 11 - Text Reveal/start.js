e07ed832be478eeca3459376dece62e429e9f559
Heyzzwatcharin <watcharinrachadet123@gmail.com>
2020-06-11 09:03:15 +0700

    Complete 11

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,4 +1,24 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  function onScroll() {
+    const sectionElems = Array.from(document.querySelectorAll("section"));
+    sectionElems.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector('img');
+      const textElem = sectionElem.querySelector('.text');
+      const scrollPosition = window.pageYOffset; //calculate position
+      const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10;
+      //แสดง 1 ส่วน 10 ของรูปภาพ  offsettop คือค่าสูงสุดของ img , offsetHeight คือค่าความสูงของรูป
+
+      if (scrollPosition >= revealPosition) {
+        textElem.classList.add('reveal');
+      }
+    });
+  }
+
+  function run() {
+    document.addEventListener("scroll", onScroll);
+  }
+
+  run();
 })();
