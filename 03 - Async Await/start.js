d619713da3f787a3e72c5cda1dc31e23a2dd4706
HeyzzWatcharin <blackf.c@hotmail.co.th>
2021-02-01 16:31:11 +0700

    Finish 04

--- 03 - Async Await/start.js
+++ 03 - Async Await/start.js
@@ -1,72 +1,71 @@
 (() => {
-  // เริ่มเขียนโค้ด
 
-  // 1. How Asynchronous code works in JavaScript
-  function similateAsyncAPI(text,timeout){ //เรียกใช้ฟังก์ชั่นหลังจาก Timeout 
-    setTimeout(() => console.log(text),timeout);
-  }
+  // function simulateAsyncAPI(text, timeout) {
+  //   setTimeout(() => console.log(text), timeout);
+  // }
 
-  similateAsynchro('A',1000); //ผ่านไป 1 วิ จะ Console.log A ออกมา
-  similateAsynchro('B',500);
-  similateAsynchro('C',100);
 
-  // 2. Callback //นิยมใน NOde JS
-  function similateAsyncAPI(text,timeout,Callback){ //callback จะเป็นฟังก์ชั่นที่ถูกเรียกใช้หลังจากฟังกชั่นของเราทำงานเสร็จแล้ว
-    setTimeout(() => {
-      console.log(text)
+  // 2.Callback
+  // function simulateAsyncAPI(text, timeout, callback) {
+  //   setTimeout(() => {
+  //     console.log(text);
+  //     if (callback) {
+  //       callback();
+  //     }
+  //   }, timeout);
 
-      if(Callback){ //เช็คว่าใน Function ที่ pass มานั้นมี callback หรือไม่ เพราะถ้าไม่ใช่เงื่อนไขจะ Error เพราะเราส่ง UNdefind มา 
-        Callback();
-      }
-    },timeout)
-  }
-  //callback hell คือการเรียก function เยอะเกิน
-  similateAsyncAPI('A',1000,()=>{
-    similateAsyncAPI('B',500,()=> {
-      similateAsyncAPI('C',100);
-    });
-  });
-  // 3. Promise
-  function similateAsyncAPI(text,timeout){ // concept ของ promise คือ ใช้  เรียกลงไปเรื่อยๆ
-     return new Promise((resolve,reject) => {
+
+  // }
+
+  // function run() {
+  //   simulateAsyncAPI('A', 1000, () => {   //calbackHell
+  //     simulateAsyncAPI('B', 500, () => {
+  //       simulateAsyncAPI('C', 100);
+  //     })
+  //   })
+  // }
+  // run();
+
+  //3.Promise แก้ปัญหา callback Hell!
+  // function simulateAsyncAPI(text, timeout) {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       if(text === 'B') reject ('B got rejected');
+  //       console.log(text);
+  //       resolve(); //บอกตัว promise ว่าการทำงานของ Async เสร็จแล้ว
+  //     }, timeout)
+  //   })
+  // }
+
+  // simulateAsyncAPI('A', 1000)
+  //   .then(() => { //then ถูกเรียกใช้เมื่อ Async ทำงานเสร็จและมีการเรียกใช้ resolve
+  //     return simulateAsyncAPI('B', 500); //ถ้า return เป็น promise อีก then จะรอให้ promise ข้างในให้ทำงานเสร็จก่อน
+  //   })
+  //   .then(() => {
+  //     return simulateAsyncAPI('C', 100);
+  //   })
+  //   .catch((error) => { // .catch จะถูกเรียกใช้เมื่อ เกิด error ภายในฟังก์ชั่น
+  //     console.log(error);
+  //   })
+
+  //Async / Await
+  function simulateAsyncAPI(text, timeout) {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-          if (text == 'B') return reject('B got rejected'); //เมื่อเรียกใช้งาน reject จะ told error from promise ให้
+        if (text === 'B') reject('B got rejected');
         console.log(text);
-        resolve(); //บอกตัว PRomise ว่า การทำงานของ Asynchronus เสร็จสิ้นแล้วและโค๊ดเราทำงานเสร็จแล้ว
-      },timeout);
-    });
+        resolve(); //บอกตัว promise ว่าการทำงานของ Async เสร็จแล้ว
+      }, timeout)
+    })
   }
 
-  similateAsyncAPI('A',1000)
-    .then(()=>{ //then คือ method ที่อยู่ใน Resolve โดย จะถูกเรียกใช้เมื่อ Asynchornus เสร็จก่อน
-      return similateAsyncAPI('B',500);
-    })
-    .then(()=>{
-      return similateAsyncAPI('C',100);
-    })
-    .catch((error)=>{ // .catch คือ method นี้จะถูกเรียกเมื่อเกิด error ขึ้นภายใน Funtion 
-        console.error(error);
-    })
-  
-  // 4. Async/Await
-  function similateAsyncAPI(text,timeout){ 
-     return new Promise((resolve,reject) => {
-      setTimeout(() => {
-        if (text == 'B') return reject('B got rejected');
-        console.log(text);
-        resolve();
-      },timeout);
-    });
-  }
-  
-  async function run(){ // นำหน้าฟังก์ชั่นต้องใส่ async และใน function ต้องมี await
-    try{ //try catch คือการ handle
-      await similateAsyncAPI('A',1000)
-      await similateAsyncAPI('B',500)
-      await similateAsyncAPI('C',100)
-    }
-    catch(error){
-      console.error(error)
+  async function run() {
+    try { //try, catch คือ การ Handle error ที่มาจาก Async
+      await simulateAsyncAPI('A', 1000);
+      await simulateAsyncAPI('B', 500);
+      await simulateAsyncAPI('C', 100);
+    } catch (error) {
+      console.error(error);
     }
   }
   run();
--- 04 - Eye Rolling/start.js
+++ 04 - Eye Rolling/start.js
@@ -1,22 +1,21 @@
 (() => {
-  // เริ่มเขียนโค้ด
-  function run(){
+  function run() {
     const bodyElem = document.querySelector('body');
-    const eyeElem  = document.querySelectorAll('.eye');
+    const eyeElems = document.querySelectorAll('.eye');
 
-    function noMousemove({pageX , pageY}) {
-     eyeElem.forEach((eyeElem) =>{
-       const {left,top} =  eyeElem.getBoundingClientRect();
-       const eyecenterx = left + eyeElem.offsetWidth/2;
-       const eyecentery = top + eyeElem.offsetHeight/2;
-       const radian = Math.atan2(pageX - eyecenterx,pageY-eyecentery);
-       const angle = (radian / Math.PI) * -180;
-       eyeElem.style.transform = `rotate(${angle}deg)`
-     });
+    function onMouseMove({ pageX: mouseX, pageY: mouseY }) {
+      eyeElems.forEach(eyeElem => {
+        const { left: eyeLeft, top: eyeTop } = eyeElem.getBoundingClientRect();
+
+        const eyeCenterX = eyeLeft + eyeElem.offsetWidth / 2;
+        const eyeCenterY = eyeTop + eyeElem.offsetHeight / 2;
+        const radian = Math.atan2(mouseX - eyeCenterX, mouseY - eyeCenterY);
+        const angle = (radian / Math.PI) * -180;
+        eyeElem.style.transform = `rotate(${angle}deg)`;
+      });
     }
 
-    bodyElem.addEventListener('mousemove',noMousemove)
+    bodyElem.addEventListener('mousemove', onMouseMove);
   }
-
   run();
 })();
