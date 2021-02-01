6a35686c14909cf66d75870a92ef3ee7b7e7e3bd
HeyzzWatcharin <blackf.c@hotmail.co.th>
2021-02-01 13:52:29 +0700

    Re start Project

--- 04 - Eye Rolling/start.js
+++ 04 - Eye Rolling/start.js
@@ -1,22 +1,3 @@
 (() => {
-  // เริ่มเขียนโค้ด
-  function run(){
-    const bodyElem = document.querySelector('body');
-    const eyeElem  = document.querySelectorAll('.eye');
 
-    function noMousemove({pageX , pageY}) {
-     eyeElem.forEach((eyeElem) =>{
-       const {left,top} =  eyeElem.getBoundingClientRect();
-       const eyecenterx = left + eyeElem.offsetWidth/2;
-       const eyecentery = top + eyeElem.offsetHeight/2;
-       const radian = Math.atan2(pageX - eyecenterx,pageY-eyecentery);
-       const angle = (radian / Math.PI) * -180;
-       eyeElem.style.transform = `rotate(${angle}deg)`
-     });
-    }
-
-    bodyElem.addEventListener('mousemove',noMousemove)
-  }
-
-  run();
 })();
--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,34 +1,3 @@
 (() => {
 
-  const konamicode = [
-    'ArrowUp',
-    'ArrowUp',
-    'ArrowDown',
-    'ArrowDown',
-    'ArrowLeft',
-    'ArrowRight',
-    'ArrowLeft',
-    'ArrowRight',
-    'b',
-    'a'
-  ]
-
-  let index = 0;
-
-  function onKeyDown(event){
-    event.key === konamicode[index] ? index++ : index = 0;  // check condition ว่า กดตรงกับ Konamicode มั้ย เขียนแบบ shot hand if-condition
-
-    console.log(event.key); //print key input 
-
-    if(konamicode.length === index){
-      startSnowing(); //Show snow function
-    }
-  }
- 
-  function run (){
-    document.addEventListener('keydown',onKeyDown); //รับ input ลูกศรเข้ามา
- }
-
- run();
-
 })();
