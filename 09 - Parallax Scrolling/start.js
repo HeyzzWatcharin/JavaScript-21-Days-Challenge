c21f469e3dc358aaecbcd3c2d8265064648bbd8a
HeyzzWatcharin <blackf.c@hotmail.co.th>
2021-02-17 15:51:30 +0700

    Finish 09-parallax

--- 09 - Parallax Scrolling/start.js
+++ 09 - Parallax Scrolling/start.js
@@ -1,3 +1,17 @@
 (() => {
+    function onScroll() {
+        const moonElem = document.querySelector('.moon');
+        const wishElem = document.querySelector('.wish');
 
+        // console.log(window.scrollY);
+        moonElem.style.transform = `translate(${window.scrollY * 0.7}%,${window.scrollY * -0.7}%)` //เลื่อนขึ้นและเลื่อนไปทางขวานะจ๊ะ
+
+        wishElem.style.transform = `translateY(${window.scrollY * -3.2}%)` //Set text scroll down
+    }
+
+    function run() {
+        document.addEventListener('scroll', onScroll)
+    }
+
+    run()
 })();
