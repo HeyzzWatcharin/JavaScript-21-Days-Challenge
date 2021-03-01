3d24c8355538eb42a800ccc9a18f03c387c6dac5
HeyzzWatcharin <blackf.c@hotmail.co.th>
2021-03-02 01:01:44 +0700

    Finish 11-text-reveal

--- 11 - Text Reveal/start.js
+++ 11 - Text Reveal/start.js
@@ -1,3 +1,24 @@
 (() => {
+    function onScroll() {
 
+        const sectionElem = Array.from(document.querySelectorAll('section'))
+        sectionElem.forEach((sectionElem) => {
+            const imgElem = sectionElem.querySelector('img')
+            const textElem = sectionElem.querySelector('.text')
+
+            const scrollPosition = window.pageYOffset //Position Y
+            const revealPosition = imgElem.offsetTop + imgElem.offsetHeight / 10 //ความสูงสุดของรูป + ความสูงของรูป หาร 10
+
+            if (scrollPosition >= revealPosition) {
+                textElem.classList.add('reveal') //Add class reveal to Text Element
+            }
+        })
+    }
+
+    function run() {
+        document.addEventListener('scroll', onScroll);
+
+    }
+
+    run();
 })();
